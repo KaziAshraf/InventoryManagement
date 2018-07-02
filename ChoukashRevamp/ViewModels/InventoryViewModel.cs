b37d418e5535bdd7d4ba8eb42a15fba31459ba2c
Ashraf.Gononet <ashraf.gononet@hotmail.com>
2018-07-02 18:18:27 +0600

    InventoryViewModel

--- ChoukashRevamp/ViewModels/InventoryViewModel.cs
+++ ChoukashRevamp/ViewModels/InventoryViewModel.cs
@@ -1,15 +1,12 @@
 ﻿using Caliburn.Micro;
 using ChoukashRevamp.Models;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.Entity;
-using System.Collections.Specialized;
 using System.Windows.Controls;
-using ChoukashRevamp.ViewModels.Navigator;
+using System.Windows.Input;
+using System.Windows;
 
 namespace ChoukashRevamp.ViewModels
 {
@@ -17,13 +14,19 @@ namespace ChoukashRevamp.ViewModels
     {
         #region Declaration
         private ObservableCollection<Category> _categories;
+        private ObservableCollection<SubCategory> _subcategories;
+
         private Choukash_Revamp_DemoEntities1 Context = new Choukash_Revamp_DemoEntities1();
         private bool _createcategory;
+        private bool _createsubcategory;
+
         private Category _currentcategory;
         private bool CategoryCreationMode;
+        private bool SubCategoryCreationMode;
 
         private IEventAggregator EventAggregator { get; set; }
         private User _user;
+        
 
         public User User
         {
@@ -42,7 +45,14 @@ namespace ChoukashRevamp.ViewModels
 
             }
         }
-
+        public bool CreateSubCategory
+        {
+                get { return _createsubcategory; }
+                set {
+                    _createsubcategory = value;
+                    NotifyOfPropertyChange(() => CreateSubCategory);
+                }
+        }
 
         public bool CreateCategory
         {
@@ -56,7 +66,14 @@ namespace ChoukashRevamp.ViewModels
 
         public Company Company { get; set; }
 
-
+        public ObservableCollection<SubCategory> SubCategories
+        {
+            get { return _subcategories; }
+            set {
+                _subcategories = value;
+                NotifyOfPropertyChange(() => SubCategories);
+            }
+        }
         public ObservableCollection<Category> Categories
         {
             get { return _categories; }
@@ -73,44 +90,25 @@ namespace ChoukashRevamp.ViewModels
             User = user;
             Company = company;
             Categories = LoadAllCategoriesofCompany;
+            
             CreateCategory = false;
+            CreateSubCategory = false;
             EventAggregator = ea;
         }
-
-       
+        #region Category
 
         private ObservableCollection<Category> LoadAllCategoriesofCompany => new ObservableCollection<Category>(Context.Categories.Include(a => a.SubCategories).ToList());
-
-        public void AddNewCategory(object sender, int index)
-        {
-            if (CategoryCreationMode == true)
-            {
-                var datagrid = sender as DataGrid;
-                var items = datagrid.ItemsSource as ObservableCollection<Category>;
-
-                if (index == items.Count - 1)
-                {
-                    if (!String.IsNullOrWhiteSpace(items.Last().name))
-                    {
-                        Context.Categories.Add(new Category() { name = items.Last().name, companies_id = Company.id, users_id = User.id });
-                        Context.SaveChanges();
-
-                        CategoryCreationMode = false;
-                    }
-                } 
-            }
-        }
 
         public void TiggerCategoryCreationMode()
         {
             CategoryCreationMode = true;
         }
-        public void ValidateCells(object sender, DataGridCellEditEndingEventArgs e)
+        public void AddEditCategory(object sender, DataGridCellEditEndingEventArgs e)
         {
+            TextBox textBox = e.EditingElement as TextBox;
+            var currentRow = e.Row.DataContext as Category;
             if (!CategoryCreationMode)
             {
-                TextBox textBox = e.EditingElement as TextBox;
-                var currentRow = e.Row.DataContext as Category;
                 if (String.IsNullOrWhiteSpace(textBox.Text))
                 {
                     e.Cancel = true;
@@ -124,17 +122,137 @@ namespace ChoukashRevamp.ViewModels
             }
             else
             {
-                TextBox textBox = e.EditingElement as TextBox;
-                var currentRow = e.Row.DataContext as Category;
+                if (!String.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    var rowIndex = e.Row.GetIndex();
+                    if (rowIndex == Categories.Count - 1)
+                    {
+                        Context.Categories.Add(new Category() { name = textBox.Text, companies_id = Company.id, users_id = User.id });
+                        Context.SaveChanges();
+
+                    }
+                    else
+                    {
+                        var editedrow = Context.Categories.Where(a => a.id == currentRow.id).SingleOrDefault();
+                        editedrow.name = textBox.Text;
+                        Context.SaveChanges();
+                    }
+                    Categories = LoadAllCategoriesofCompany;
+                }
+                else
+                    e.Cancel = true;
+            }
+        }
+
+        public void DeleteCategory(object sender, KeyEventArgs e)
+        {
+            var datagrid = sender as DataGrid;
+            var row = datagrid.ItemContainerGenerator.ContainerFromIndex(datagrid.SelectedIndex) as DataGridRow;
+            if (Key.Delete == e.Key && !row.IsEditing && !row.IsNewItem)
+            {
+                var result = MessageBox.Show("Do You want to delete this category?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Context.Categories.Remove(datagrid.SelectedItem as Category);
+                    Context.SaveChanges();
+                    Categories = LoadAllCategoriesofCompany;
+                }
+                else
+                    e.Handled = true;
+            }
+            else if (Key.Escape == e.Key)
+            {
+                Categories = LoadAllCategoriesofCompany;
+                CreateCategory = false;
 
             }
         }
+        #endregion
+
+        #region SubCategory
+        public void LoadSubCategoryFromCategory(Category category)
+        {
+            SubCategories = new ObservableCollection<SubCategory>(Context.SubCategories.Where(a => a.categories_id == category.id).ToList());
+        }
+        public void TiggerSubCategoryCreationMode()
+        {
+            SubCategoryCreationMode = true;
+        }
+        public void AddEditSubCategory(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            TextBox textBox = e.EditingElement as TextBox;
+            var currentRow = e.Row.DataContext as SubCategory;
+            if (!SubCategoryCreationMode)
+            {
+                if (String.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    var editedrow = Context.SubCategories.Where(a => a.id == currentRow.id).SingleOrDefault();
+                    editedrow.name = textBox.Text;
+                    Context.SaveChanges();
+                }
+            }
+            else
+            {
+                if (!String.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    var rowIndex = e.Row.GetIndex();
+                    if (rowIndex == Categories.Count - 1)
+                    {
+                        Context.Categories.Add(new Category() { name = textBox.Text, companies_id = Company.id, users_id = User.id });
+                        Context.SaveChanges();
+
+                    }
+                    else
+                    {
+                        var editedrow = Context.Categories.Where(a => a.id == currentRow.id).SingleOrDefault();
+                        editedrow.name = textBox.Text;
+                        Context.SaveChanges();
+                    }
+                    Categories = LoadAllCategoriesofCompany;
+                }
+                else
+                    e.Cancel = true;
+            }
+        }
+
+        public void DeleteSubCategory(object sender, KeyEventArgs e)
+        {
+            var datagrid = sender as DataGrid;
+            var row = datagrid.ItemContainerGenerator.ContainerFromIndex(datagrid.SelectedIndex) as DataGridRow;
+            if (Key.Delete == e.Key && !row.IsEditing && !row.IsNewItem)
+            {
+                var result = MessageBox.Show("Do You want to delete this category?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Context.Categories.Remove(datagrid.SelectedItem as Category);
+                    Context.SaveChanges();
+                    Categories = LoadAllCategoriesofCompany;
+                }
+                else
+                    e.Handled = true;
+            }
+            else if (Key.Escape == e.Key)
+            {
+                Categories = LoadAllCategoriesofCompany;
+                CreateCategory = false;
+
+            }
+        }
+        #endregion
+
         public void EnableGridtoAddRow(string entity)
         {
             switch (entity)
             {
                 case "Categories":
                     CreateCategory = true;
+                    break;
+                case "SubCategories":
+                    CreateSubCategory = true;
                     break;
             }
         }
